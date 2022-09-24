f5fcefc6cdc1c6b8ca9a7c33a0b30be3bad90dbe
BekhterNikolay <n.v.bekhter@mail.ru>
2022-09-24 23:58:39 +0300

    реализован консольный терминал на NIO поддержаны команды: "ls", "touch", "mkdir", "cat".

--- nio/src/main/java/ru/geekbrains/server/TelnetTerminal.java
+++ nio/src/main/java/ru/geekbrains/server/TelnetTerminal.java
@@ -10,6 +10,7 @@ import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -51,41 +52,87 @@ public class TelnetTerminal {
 
     private void handleRead(SelectionKey key) throws IOException {
         SocketChannel channel = (SocketChannel) key.channel();
+        buf.clear();
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int read = channel.read(buf);
+            if (read == 0) {
+                break;
+            }
+            if (read == -1) {
+                channel.close();
+                return;
+            }
+            buf.flip();
+            while (buf.hasRemaining()) {
+                sb.append((char) buf.get());
+            }
             buf.clear();
-            StringBuilder sb = new StringBuilder();
-            while (true) {
-                int read = channel.read(buf);
-                if (read == 0) {
-                    break;
-                }
-                if (read == -1) {
-                    channel.close();
-                    return;
-                }
-                buf.flip();
-                while (buf.hasRemaining()) {
-                    sb.append((char) buf.get());
-                }
-                buf.clear();
 
-                System.out.println("Received: " + sb);
-                String command = sb.toString().trim();
-                if (command.equals("ls")) {
-                    String files = Files.list(current)
-                            .map(p -> p.getFileName().toString())
-                            .collect(Collectors.joining("\n\r"));
-                    channel.write(ByteBuffer.wrap(files.getBytes(StandardCharsets.UTF_8)));
+            System.out.println("Received: " + sb);
+            String command = sb.toString().trim();
+            if (command.equals("ls")) {
+                String files = Files.list(current)
+                        .map(p -> p.getFileName().toString())
+                        .collect(Collectors.joining("\n\r"));
+                channel.write(ByteBuffer.wrap(files.getBytes(StandardCharsets.UTF_8)));
+            } else if (command.startsWith("cd")) {
+                command = command.replaceAll("cd ", "").trim();
+                if (command.equals("..")) {
+                    if (current.getParent() == null) {
+                        current = current.toAbsolutePath();
+                    }
+                    current = current.getParent();
+                } else if (Files.isDirectory(current.resolve(command))) {
+                    current = current.resolve(command);
+                } else if (Files.isDirectory(Paths.get(command)) && Files.exists(Paths.get(command))) {
+                    current = Paths.get(command);
                 } else {
                     byte[] bytes = command.getBytes(StandardCharsets.UTF_8);
                     channel.write(ByteBuffer.wrap(bytes));
                 }
+            } else if (command.startsWith("cat")) {
+                command = command.replaceAll("cat ", "").trim();
+                command = String.valueOf(Path.of(current.toAbsolutePath() + "\\" + command));
+                if (Files.isDirectory(Paths.get(command))) {
+                    command = "The command 'cat' is applicable to files.";
+                    byte[] bytes = command.getBytes(StandardCharsets.UTF_8);
+                    channel.write(ByteBuffer.wrap(bytes));
+                } else if (Files.exists(Paths.get(command))) {
+                    byte[] bytes = Files.readAllBytes(Path.of(command));
+                    channel.write(ByteBuffer.wrap(bytes));
+                }
+            } else if (command.startsWith("mkdir")) {
+                command = command.replaceAll("mkdir ", "").trim();
+                Path path = Path.of(current.toAbsolutePath() + "\\" + command);
+                if (!Files.exists(path)) {
+                    Files.createDirectory(path);
+                }
+            } else if (command.startsWith("touch")) {
+                command = command.replaceAll("touch ", "").trim();
+                Path path = Path.of(current.toAbsolutePath() + "\\" + command);
+                if (!Files.exists(path)) {
+                    Files.createFile(path);
+                }
+            } else {
+                byte[] bytes = command.getBytes(StandardCharsets.UTF_8);
+                channel.write(ByteBuffer.wrap(bytes));
             }
+        }
+        printPath(channel);
+    }
+
+    private void printPath(SocketChannel channel) throws IOException {
+        String path = "\n\r" + current.toString() + " ";
+        byte[] bytes = path.getBytes(StandardCharsets.UTF_8);
+        channel.write(ByteBuffer.wrap(bytes));
     }
 
     private void handleAccept() throws IOException {
         SocketChannel socketChannel = server.accept();
         socketChannel.configureBlocking(false);
         socketChannel.register(selector, SelectionKey.OP_READ);
+        printPath(socketChannel);
         System.out.println("Client accepted");
     }
 
